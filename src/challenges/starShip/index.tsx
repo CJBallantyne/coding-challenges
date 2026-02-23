8af73764c068a57e070decfa4c2cbba34cd01f6c
Connor Ballantyne <connor.ballantyne@menlolab.com>
2026-02-23 11:29:17 -0500

    increase system size

--- src/challenges/starShip/index.tsx
+++ src/challenges/starShip/index.tsx
@@ -3,7 +3,7 @@ import p5 from "p5";
 import { Star } from "./star";
 import { Ship } from "./ship";
 import { ViewPort } from "./viewPort";
-const SYSTEM_SIZE = 5000;
+export const SYSTEM_SIZE = 10000;
 const CANVAS_SIZE = 1000;
 const stars: Star[] = [];
 const ship = new Ship(new p5.Vector(SYSTEM_SIZE / 2, SYSTEM_SIZE / 2));
@@ -18,7 +18,7 @@ export const starSystem = () => {
   const sketch = (p: p5) => {
     p.setup = () => {
       p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-      for (let i = 0; i < 500; i++) {
+      for (let i = 0; i < SYSTEM_SIZE / 2; i++) {
         stars.push(
           new Star(
             new p5.Vector(
